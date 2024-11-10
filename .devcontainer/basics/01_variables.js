e09c7b52b451f7567064f575f9d48669944419a4
Ravi Sharma <paarth.ravi234@gmail.com>
2024-11-10 17:31:51 +0000

    first commit

---
+++ .devcontainer/basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 273314
+let accountEmail = "ravi4536@gmail.com"
+var accountPassword="1232"
+//accountCity="Madhubani"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
