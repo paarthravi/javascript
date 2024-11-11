ff2003072f5ce301c278e3f35e736ba40abcd025
Ravi Sharma <paarth.ravi234@gmail.com>
2024-11-11 02:44:47 +0000

    day2

---
+++ .devcontainer/basics/02_variables.js
@@ -0,0 +1,16 @@
+"use strict"; //treact all js code syntax as newer version
+
+//alert(hello) //we are using nodejs not browser
+
+//NUMBER= 2 TO POWER 53
+//BIGiNT
+//STRING
+//Boolean => true/false
+//null => stand alone value 
+//undefined=>
+//symbol
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
