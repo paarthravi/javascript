e36ff28738fa5c06c5094a10a2768eb1a1125e75
Ravi Sharma <paarth.ravi234@gmail.com>
2024-11-13 03:19:22 +0000

    day2

---
+++ .devcontainer/basics/operations.js
@@ -0,0 +1,23 @@
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+//********************Comparison*************************************//
+console.log(null > 0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+
+//strict check
+console.log("2"==2);//true
+console.log("2"===2);//false
+
+//888complted
+
+
+
+
