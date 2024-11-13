06d0b00763988ad50bae1fa6e107b8ad704d8d3a
Ravi Sharma <paarth.ravi234@gmail.com>
2024-11-13 02:57:53 +0000

    day 2

---
+++ .devcontainer/basics/datatype_conversion.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+console.log(typeof(score));
+
+//conversion
+let ValueInNumber =     Number(score);
+console.log(typeof ValueInNumber);
+
+//"33" => 33
+//"33abc"=>NaN
+//true=>1 and false=>0
+
+
+let isLoggedIn = 0;
+
+let BooleanLoggedIn =  Boolean(isLoggedIn)
+console.log(BooleanLoggedIn)
+
+
+//1=>true
+//0=>false
+//"ravi"=>true
+
